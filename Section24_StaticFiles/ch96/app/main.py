c044141782d8791534da2c94d036972c153b663c
TalhaMudassar <talhamudassar200@gmail.com>
2026-04-11 00:29:39 +0500

    static files

---
+++ Section24_StaticFiles/ch96/app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+@app.get("/items")
+async def all_items():
+    return all_items
