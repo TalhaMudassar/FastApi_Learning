420d3c6c57f137a20dba4e4944813201889634e2
TalhaMudassar <talhamudassar200@gmail.com>
2026-05-08 22:27:33 +0500

    Get User Access (Authentication)

--- Section27_Authentication_Authorization/ch106/app/account/routers.py
+++ Section27_Authentication_Authorization/ch106/app/account/routers.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from app.account.services import create_user, authenticate_user
 from app.account.models import UserCreate, UserOut
 from app.db.config import SessionDep
@@ -6,6 +6,7 @@ from fastapi.security import OAuth2PasswordRequestForm
 from fastapi import HTTPException
 from app.account.utils import create_tokens, verify_refresh_token
 from fastapi.responses import JSONResponse
+from app.account.dependencies import get_current_user
 
 
 router = APIRouter(prefix="/account" , tags=["Account"])
@@ -37,3 +38,6 @@ def refresh_token(session: SessionDep, request: Request):
     raise HTTPException(status_code=401, detail="Invalid or expired refresh token")
   return create_tokens(session, user)
  
+@router.get("/me", response_model=UserOut)
+def me(user = Depends(get_current_user)):
+  return user
